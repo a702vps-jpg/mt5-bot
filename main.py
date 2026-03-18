49d60fd08642fde18d6b5b7795a2d76c49098363
a702vps-jpg <a702vps@gmail.com>
2026-03-19 03:25:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import time
+from telethon import TelegramClient, events
+import os
 
-print("Bot ishlayapti 🚀")
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
 
-while True:
-    print("Ishlayapti...")
-    time.sleep(10)
+client = TelegramClient("session", api_id, api_hash)
+
+@client.on(events.NewMessage)
+async def handler(event):
+    print("Yangi signal:", event.text)
+
+client.start()
+print("Telegram bot ishlayapti 🚀")
+
+client.run_until_disconnected()
